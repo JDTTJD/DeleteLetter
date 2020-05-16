b81657b24c44f81f10f3bf3c08b6d5d917f9e0fe
Lenovo <820231422@qq.com>
2020-05-16 18:26:21 +0800

    搜索框加删除按钮

---
+++ app/src/main/java/com/tuoren/deleteletter/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.tuoren.deleteletter
+
+import android.app.Activity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ivDeleteText.setOnClickListener {
+            this@MainActivity.etSearch.setText("")
+        }
+        this.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 0) {
+                    this@MainActivity.ivDeleteText.visibility = View.INVISIBLE
+                } else {
+                    this@MainActivity.ivDeleteText.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
+}
